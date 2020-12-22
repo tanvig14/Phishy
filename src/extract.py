a6a617747fb67b7a9f37d1f2211f22ef403c7695
dev-ved30 <vedgs2@illinois.edu>
2020-12-21 20:27:39 -0500

    Added extractDoubleSlashRedirecting

--- src/extract.py
+++ src/extract.py
@@ -5,10 +5,11 @@ elements = urlparse(URL)
 
 
 features = {
-    "having_IP_Address":0,
-    "port":0,
-    "URL_Length":0,
-    "having_At_Symbol":0,
+    "having_IP_Address": 0,
+    "port": 0,
+    "URL_Length": 0,
+    "having_At_Symbol": 0,
+    "double_slash_redirecting": 0,
 }
 
 ports = {
@@ -27,21 +28,50 @@ ports = {
 def extractPort():
     port = ports[elements.scheme]
     print(port)
-    ## Wait till we implement pure URL feature
+    ## Wait till we implement pure URL features
 
 def extractUrlLength():
+    """
+    Sets the URL_Length feature with the threshold value of 54.
+
+    1) -1 if the length is greater than or equal to 54.
+    2) 1 if the length is less than 54.
+    """
+
     if (len(URL) >= 54 ):
         features['URL_Length'] = -1
     else:
         features['URL_Length'] = 1
 
 def extractAtSymbol():
+    """
+    Sets the having_At_Symbol feature based on the presence of @ in the URL
+
+    1) -1 if the URL contains @
+    2) 1 if the URL does not contain @
+    """
+
     if "@" in URL:
         features["having_At_Symbol"] = -1
     else:
         features["having_At_Symbol"] = 1
 
+def extractDoubleSlashRedirecting():
+    """
+    Sets the double_slash_redirecting feature based on the position of the last //.
+
+    1) -1 if the last occuring // is after the 6th index.
+    2) 1 if the last occuring // is before the 6th index.
+    """
+    
+    if URL.rfind("//") > 6:
+        features["double_slash_redirecting"] = -1
+    else:
+        features["double_slash_redirecting"] = 1
+
+
 extractPort()
 extractAtSymbol()
 extractUrlLength()
+extractDoubleSlashRedirecting()
 print(features)
