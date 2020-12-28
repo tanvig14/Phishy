2aa6ee2be50bf2f5537cb3190a87e67e646a2e72
dev-ved30 <vedgs2@illinois.edu>
2020-12-27 21:05:33 -0500

    Data is now deocoded correctly

--- src/model.py
+++ src/model.py
@@ -2,10 +2,9 @@ import numpy as np
 import pandas as pd
 from scipy.io.arff import loadarff 
 
-raw_data = loadarff(open('train.arff','r'))
-data = pd.DataFrame(raw_data[0])
-data["Result"] = str(data["Result"],'utf-8')
-print(data)
-c1 = data["URL_Length"]
-c2 = data["Result"]
-print(c1.corr(c2))
+raw_data = loadarff(open('../data/train.arff','r',encoding='UTF-8'))
+df = pd.DataFrame(raw_data[0])
+str_df = df.select_dtypes([np.object]) 
+int_df = str_df.stack().str.decode('utf-8').unstack().astype(int)
+print(int_df)
+
