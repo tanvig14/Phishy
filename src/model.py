c19d2525fe384929b4b6506760b86aec4f11f5e6
dev-ved30 <vedgs2@illinois.edu>
2021-01-04 21:26:51 -0500

    Dropped domain registration length feature

--- src/model.py
+++ src/model.py
@@ -20,25 +20,43 @@ droppedFeatures = [
     'on_mouseover',
     'RightClick',
     'popUpWidnow',
-    'Iframe']
+    'Iframe',
+    'Domain_registeration_length'
+    ]
 
 # Loading data and decoding it appropriately.
 raw_data = loadarff(open('../data/train.arff','r',encoding='UTF-8'))
 df = pd.DataFrame(raw_data[0])
 str_df = df.select_dtypes([np.object]) 
 int_df = str_df.stack().str.decode('utf-8').unstack().astype(int)
-print(int_df)
 
 # Dropping the features which are not going to be used from the data frame.
 int_df.drop(droppedFeatures, axis = 1, inplace = True)
-int_df.replace(0,-1)
+int_df.replace(0,-1,inplace = True)
+
+correlation = {
+    "having_IP_Address": int_df["having_IP_Address"].corr(int_df["Result"]),
+    "port": int_df["port"].corr(int_df["Result"]),
+    "URL_Length": int_df["URL_Length"].corr(int_df["Result"]),
+    "having_At_Symbol": int_df["having_At_Symbol"].corr(int_df["Result"]),
+    "double_slash_redirecting": int_df["double_slash_redirecting"].corr(int_df["Result"]),
+    "Prefix_Suffix": int_df["Prefix_Suffix"].corr(int_df["Result"]),
+    "Redirect": int_df["Redirect"].corr(int_df["Result"]),
+    "HTTPS_token": int_df["HTTPS_token"].corr(int_df["Result"]),
+    "Shortining_Service": int_df["Shortining_Service"].corr(int_df["Result"]),
+    "having_Sub_Domain": int_df["having_Sub_Domain"].corr(int_df["Result"]),
+    "having_IP_Address": int_df["having_IP_Address"].corr(int_df["Result"]),
+    "SSLfinal_State": int_df["SSLfinal_State"].corr(int_df["Result"]),
+    "age_of_domain": int_df["age_of_domain"].corr(int_df["Result"]),
+    "Favicon": int_df["Favicon"].corr(int_df["Result"]),
+    "Submitting_to_email": int_df["Submitting_to_email"].corr(int_df["Result"])}
 
 # Loading values
-x = int_df.values[:,0:15]
-y = int_df.values[:,15]
+x = int_df.values[:,0:14]
+y = int_df.values[:,14]
 
 # Splitting the dataset for training and testing.
-x_tr, x_ts, y_tr, y_ts = train_test_split(x,y,test_size=0.20)
+x_tr, x_ts, y_tr, y_ts = train_test_split(x,y,test_size=0.30)
 
 # Fitting the training data
 model = SVC()
@@ -50,6 +68,7 @@ predict = model.predict(x_ts)
 # Printing results
 print("Accuracy: ",accuracy_score(y_ts, predict))
 print("F1 Score: ",f1_score(y_ts, predict))
+print(correlation)
 
 # Saving model
-pickle.dump(model, open('final_model.sav','wb'))
+pickle.dump(model, open('weighed_final_model.sav','wb'))
