a569ace47b73a972ef7049878aa49ac63266e4ec
dev-ved30 <vedgs2@illinois.edu>
2021-01-07 16:24:15 -0500

    Changes to the website

--- WebSite/apiCall.js
+++ WebSite/apiCall.js
@@ -1,25 +1,28 @@
+
 $(document).ready(function() {
-    $("#submiturl").click(function () {
-      var url = $("#url").val();
+  $("#submiturl").click(function () {
+    var url = $("#url").val();
+    document.getElementById('result').src = "images/processing.gif";
 
-
-      var request = $.ajax({
-        url: "http://127.0.0.1:5000/adv_results/",
-        method: "GET",
-        dataType: "json",
-        data: {
-          url: url
-        }
-      });
-
-      request.done (function (msg) {
-        console.log(msg);
-      });
-
-      request.fail (function (jqXHR, testStatus) {
-        alert("Data transfer failed!");
-      });
-
+    var request = $.ajax({
+      url: "http://127.0.0.1:5000/adv_results/",
+      method: "GET",
+      dataType: "json",
+      data: {
+        url: url
+      }
     });
 
-   });
+    request.done (function (msg) {
+      console.log(msg);
+      document.getElementById('result').src = "images/loading (1).gif";
+      $('#report').html("Incorrectly Reported?<br><input type=\"submit\" id=\"submiturl\" value=\"Report a problem\"/>");
+    });
+
+    request.fail (function (jqXHR, testStatus) {
+      alert("Data transfer failed!");
+    });
+
+  });
+
+ });
